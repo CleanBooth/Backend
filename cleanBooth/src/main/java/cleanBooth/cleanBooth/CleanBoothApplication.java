efb1ae5f34174355185599a52c4f178c39ca3ae7
HyunWoo <kimjd766@gmail.com>
2023-07-21 22:58:00 +0900

    feat : 프로젝트 생성

---
+++ cleanBooth/src/main/java/cleanBooth/cleanBooth/CleanBoothApplication.java
@@ -0,0 +1,13 @@
+package cleanBooth.cleanBooth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanBoothApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanBoothApplication.class, args);
+	}
+
+}
