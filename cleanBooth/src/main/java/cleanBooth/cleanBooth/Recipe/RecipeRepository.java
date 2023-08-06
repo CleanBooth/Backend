27a58b575261102f060285f3f90d6add23fd2a35
nowuri <be2110421@sookmyung.ac.kr>
2023-08-06 19:26:16 +0900

    fetch: Recipe 패키지 생성 및 파일 이동

--- cleanBooth/src/main/java/cleanBooth/cleanBooth/Recipe/RecipeRepository.java
+++ cleanBooth/src/main/java/cleanBooth/cleanBooth/Recipe/RecipeRepository.java
@@ -1,19 +1,10 @@
-package cleanBooth.cleanBooth.repository;
+package cleanBooth.cleanBooth.Recipe;
 
 import cleanBooth.cleanBooth.domain.Recipe;
-import cleanBooth.cleanBooth.service.RecipeService;
 import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
-import org.hibernate.engine.jdbc.connections.spi.ConnectionProvider;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.datasource.ConnectionProxy;
 import org.springframework.stereotype.Repository;
-import org.springframework.ui.Model;
 
-import javax.xml.transform.Result;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
--- cleanBooth/src/main/java/cleanBooth/cleanBooth/Recipe/RecipeService.java
+++ cleanBooth/src/main/java/cleanBooth/cleanBooth/Recipe/RecipeService.java
@@ -1,14 +1,12 @@
-package cleanBooth.cleanBooth.service;
+package cleanBooth.cleanBooth.Recipe;
 
 
 import cleanBooth.cleanBooth.domain.Recipe;
-import cleanBooth.cleanBooth.repository.RecipeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
