daf6708f81bf8f95ae79970098998a98ca5c3944
HyunWoo <kimjd766@gmail.com>
2023-08-24 11:57:24 +0900

    feat:리뷰 작성, 수정 추가

--- cleanBooth/src/main/java/cleanBooth/cleanBooth/Item/Detail/Controller/ItemDetailController.java
+++ cleanBooth/src/main/java/cleanBooth/cleanBooth/Item/Detail/Controller/ItemDetailController.java
@@ -1,10 +1,12 @@
 package cleanBooth.cleanBooth.Item.Detail.Controller;
 
 import cleanBooth.cleanBooth.Item.Detail.Dto.ItemDetailResponseDto;
-import cleanBooth.cleanBooth.Item.Detail.Dto.ItemReviewDto;
+import cleanBooth.cleanBooth.Item.Detail.Dto.PostReviewDto;
 import cleanBooth.cleanBooth.Item.Detail.Service.ItemDetailService;
-import com.mysql.cj.x.protobuf.MysqlxDatatypes;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -15,17 +17,43 @@ public class ItemDetailController {
 
     private final ItemDetailService itemDetailService;
 
+    @Autowired
+    private HttpServletRequest request;
+
+    @GetMapping("/extract-token")
+    public String extractToken() {
+        String authorizationHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            String accessToken = authorizationHeader.substring(7); // "Bearer " 이후의 문자열 추출
+            return "Extracted access token: " + accessToken;
+        }
+
+        return null;
+    }
+
     @GetMapping("items/{itemId}/{orderBy}/")
     public ItemDetailResponseDto getItemDetail(
             @PathVariable("itemId")Long itemId,
             @PathVariable("orderBy")String orderBy) {
-        return itemDetailService.findItemDetails(itemId, orderBy);
+        String accessToken = extractToken();
+        return itemDetailService.findItemDetails(itemId, orderBy, accessToken);
     }
 
-//    @PostMapping("items/{itemId}/")
-//    public ItemReviewDto postItemReview(
-//            @PathVariable("itemId")Long itemId, @RequestBody HashMap<String, Object> map){
-//
-//    }
+    @PostMapping("items/{itemId}/")
+    public PostReviewDto postItemReview(
+            @PathVariable("itemId")Long itemId,
+            @RequestBody HashMap<String, Object> map){
+        String accessToken = extractToken();
+        return itemDetailService.postReviewDto(itemId, map, accessToken);
+    }
+
+    @PatchMapping("items/{itemId}/")
+    public PostReviewDto patchItemReview(
+            @PathVariable("itemId")Long itemId,
+            @RequestBody HashMap<String, Object> map){
+        String accessToken = extractToken();
+        return itemDetailService.postReviewDto(itemId, map, accessToken);
+    }
 }
 
--- cleanBooth/src/main/java/cleanBooth/cleanBooth/Item/Detail/Dto/PostReviewDto.java
+++ cleanBooth/src/main/java/cleanBooth/cleanBooth/Item/Detail/Dto/PostReviewDto.java
@@ -2,7 +2,9 @@ package cleanBooth.cleanBooth.Item.Detail.Dto;
 
 import cleanBooth.cleanBooth.domain.Review;
 import lombok.Builder;
+import lombok.Getter;
 
+@Getter
 public class PostReviewDto {
     private String goodDescription;
     private String badDescription;
