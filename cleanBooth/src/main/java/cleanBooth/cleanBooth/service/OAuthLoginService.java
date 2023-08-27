72571ae6355dd499cdfa10c775e6da50f4cefe00
Dori <sky980221@naver.com>
2023-08-27 23:18:40 +0900

    fix: newMember -> newUser로 변경

--- cleanBooth/src/main/java/cleanBooth/cleanBooth/service/OAuthLoginService.java
+++ cleanBooth/src/main/java/cleanBooth/cleanBooth/service/OAuthLoginService.java
@@ -17,17 +17,17 @@ public class OAuthLoginService {
 
     public AuthTokens login(OAuthLoginParams params) {
         OAuthInfoResponse oAuthInfoResponse = requestOAuthInfoService.request(params);
-        Long memberId = findOrCreateMember(oAuthInfoResponse);
-        return authTokensGenerator.generate(memberId);
+        Long userId = findOrCreateUser(oAuthInfoResponse);
+        return authTokensGenerator.generate(userId);
     }
 
-    private Long findOrCreateMember(OAuthInfoResponse oAuthInfoResponse) {
+    private Long findOrCreateUser(OAuthInfoResponse oAuthInfoResponse) {
         return userRepository.findByEmail(oAuthInfoResponse.getEmail())
                 .map(User::getId)
-                .orElseGet(() -> newMember(oAuthInfoResponse));
+                .orElseGet(() -> newUser(oAuthInfoResponse));
     }
 
-    private Long newMember(OAuthInfoResponse oAuthInfoResponse) {
+    private Long newUser(OAuthInfoResponse oAuthInfoResponse) {
         User user = User.builder()
                 .name(oAuthInfoResponse.getName())
                 .email(oAuthInfoResponse.getEmail())
