99c2c97beface4d1495058fc6fade7a2939d4eee
nowuri <be2110421@sookmyung.ac.kr>
2023-09-09 02:16:34 +0900

    update: '/recipe/writer/{writer_name}' -> '/recipe/blog_writer/{writer_name}', '/recipe/youtube_writer/{writer_name}'

--- cleanBooth/src/main/java/cleanBooth/cleanBooth/repository/RecipeRepository.java
+++ cleanBooth/src/main/java/cleanBooth/cleanBooth/repository/RecipeRepository.java
@@ -64,7 +64,7 @@ public class RecipeRepository {
 
     //site의 writer 기준으로 recipe return
     public List<String> findBySite(Site site){
-        String hql = "select r.recipeWriter.name from Recipe r where r.site = :site";
+        String hql = "select DISTINCT r.recipeWriter.name from Recipe r where r.site = :site";
         TypedQuery<String> query = entityManager.createQuery(hql, String.class);
         query.setParameter("site", site);
 
@@ -94,8 +94,18 @@ public class RecipeRepository {
     }
 
     //writer 검색
-    public List<Recipe> getRecipeSearchWriter(String writer_name){
-        String hql = "select r from Recipe r where r.recipeWriter.name = :writer_name";
+    public List<Recipe> getRecipeSearchWriterByYoutube(String writer_name){
+        String hql = "select r from Recipe r where r.recipeWriter.name = :writer_name and r.site = 'Youtube'";
+        TypedQuery<Recipe> query = entityManager.createQuery(hql, Recipe.class);
+        query.setParameter("writer_name", writer_name);
+
+        List<Recipe> recipeWriterDtos = query.getResultList();
+
+        return recipeWriterDtos;
+    }
+
+    public List<Recipe> getRecipeSearchWriterByBlog(String writer_name){
+        String hql = "select r from Recipe r where r.recipeWriter.name = :writer_name and r.site = 'Blog'";
         TypedQuery<Recipe> query = entityManager.createQuery(hql, Recipe.class);
         query.setParameter("writer_name", writer_name);
 
